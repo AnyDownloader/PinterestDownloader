7f49bdd614d450d309e378ea58eb75300d4a6dd4
alexpin <intracomof@gmail.com>
2021-07-23 12:49:42 +0700

    [compatibility] getExtSource

--- src/Model/PinterestFetchedResource.php
+++ src/Model/PinterestFetchedResource.php
@@ -3,4 +3,13 @@ namespace AnyDownloader\PinterestDownloader\Model;
 
 use AnyDownloader\DownloadManager\Model\FetchedResource;
 
-final class PinterestFetchedResource extends FetchedResource { }
+final class PinterestFetchedResource extends FetchedResource
+{
+    /**
+     * @return string
+     */
+    public function getExtSource(): string
+    {
+        return 'pinterest';
+    }
+}
